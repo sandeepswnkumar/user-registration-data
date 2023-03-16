246921a3c6b923286db989e6286d0491105c4b57
SANDEEP KUMAR GUPTA <sandeepswnkumar@gmail.com>
2023-03-17 00:53:51 +0530

    update backend link

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosBase = axios.create({
-    baseURL: 'https://user-registration-data.herokuapp.com/'
+    baseURL: 'https://user-management-backend-10lu.onrender.com/'
 });
 
 
